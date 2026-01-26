e3ca6e14c9753ea2ab0d411f0f6011a45e95c58e
Le3798 <bakhoaleingermany@gmail.com>
2026-01-26 15:48:35 +0100

    Add manga database

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,20 +1,26 @@
 /// <reference types="@cloudflare/workers-types" />
 
 declare global {
-	namespace App {
-		// interface Error {}
-		interface Locals {
-			db: Record<string, D1Database>;
-		}
-		// interface PageData {}
-		interface Platform {
-			env: {
-				SHOW_INTERNAL_TABLES?: string;
-				OPENAI_API_KEY?: string;
-				AI?: unknown;
-			} & Record<string, Fetcher | string>;
-		}
-	}
+    namespace App {
+        // interface Error {}
+        interface Locals {
+            db: Record<string, D1Database>;
+        }
+        // interface PageData {}
+        interface Platform {
+            env: {
+                SHOW_INTERNAL_TABLES?: string;
+                OPENAI_API_KEY?: string;
+                AI?: unknown;
+                
+                // Add your databases here so TS knows they are D1Databases
+                Manga: D1Database;
+                // (Add your other library DB name here too if you want)
+                
+            } & Record<string, D1Database | Fetcher | string>; // Updated to include D1Database
+        }
+    }
 }
-
 export {};
+
+
